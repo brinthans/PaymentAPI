0fb4b2528f207929d758599e3af49f2d191fc440
Brinthan <brinthsega@gmail.com>
2020-12-17 12:51:55 +0530

    added model

--- PaymentAPI/Models/PaymentDetail.cs
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,16 @@ namespace PaymentAPI.Models
         [Key]
         public int PaymentDetailId { get; set; }
 
+        [Column(TypeName = "nvarchar(100)")]
         public string CardOwnerName { get; set; }
 
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpiryDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
     }
 }
