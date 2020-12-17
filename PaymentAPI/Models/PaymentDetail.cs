345d5801aa9ce98b9ca32bb95abd728ee400722c
Brinthan <brinthsega@gmail.com>
2020-12-17 12:32:30 +0530

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        public string CardOwnerName { get; set; }
+
+    }
+}
