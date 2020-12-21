a035d64bdba94bc90e2bb09ca943d06396561f87
Brinthan <brinthsega@gmail.com>
2020-12-21 22:53:04 +0530

    added cors

--- PaymentAPI/Startup.cs
+++ PaymentAPI/Startup.cs
@@ -27,7 +27,6 @@ namespace PaymentAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -41,6 +40,11 @@ namespace PaymentAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
